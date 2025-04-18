f5ec81daecdf708155f466b2218b02f2be48c7f2
Sungjun Lee <60735372+sungjunleeee@users.noreply.github.com>
2025-04-18 11:22:07 -0400

    Refactored code and added chrome_options

--- har.py
+++ har.py
@@ -1,38 +1,44 @@
-# from selenium import webdriver
-# import undetected_chromedriver as uc
-from seleniumwire import webdriver
-
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-import time
-import json, datetime, pathlib
+from seleniumwire import webdriver  # Import Selenium Wire's webdriver
+import time, pathlib
 
 """
 Set up Chrome Driver
 """
-
-options = {"enable_har": True}
-
-driver = webdriver.Chrome(seleniumwire_options=options)
+# Configure Chrome options if needed (here, we keep it basic)
+chrome_options = webdriver.ChromeOptions()
 
 """
-Youtube
+Set Selenium Wire options
+"""
+seleniumwire_options = {
+    "enable_har": True
+}  # This enables HAR recording of all network traffic
+
+driver = webdriver.Chrome(
+    options=chrome_options, seleniumwire_options=seleniumwire_options
+)
+
+"""
+Visiting YouTube
 """
 # The driver waits until the page is fully loaded
-driver.get("https://www.youtube.com")
+url = "https://www.youtube.com"
+driver.get(url)
 
 # Wait for the driver to log more network activities after it's fully loaded
-time.sleep(20)
+wait_time = 20
+print(f"Waiting for {wait_time} on {url}")
+time.sleep(wait_time)
 
-har = driver.har
+# After browsing, retrieve the HAR data from Selenium Wire
+har_data = driver.har
 
-timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+# Export the HAR data to a file in JSON format
+timestamp = time.strftime("%Y%m%d-%H%M%S", time.localtime())
 outfile = pathlib.Path(f"youtube_{timestamp}.har.json")
-with outfile.open("w", encoding="utf-8") as f:
-    f.write(har)
-    f.flush()
+with outfile.open("w", encoding="utf-8") as har_file:
+    har_file.write(har_data)
+    har_file.flush()
 
 print(f"HAR written to {outfile.resolve()}")
 
