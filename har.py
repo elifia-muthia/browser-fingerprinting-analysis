f457aaacc8b77455c7d433ff020ae54f1e98f8eb
Sungjun Lee <60735372+sungjunleeee@users.noreply.github.com>
2025-04-18 04:38:59 -0400

    Removed JSONification

--- har.py
+++ har.py
@@ -1,6 +1,7 @@
 # from selenium import webdriver
 # import undetected_chromedriver as uc
 from seleniumwire import webdriver
+
 # from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.chrome.service import Service
@@ -12,25 +13,26 @@ import json, datetime, pathlib
 Set up Chrome Driver
 """
 
-options = {
-    'enable_har': True
-}
+options = {"enable_har": True}
 
 driver = webdriver.Chrome(seleniumwire_options=options)
 
 """
 Youtube
 """
+# The driver waits until the page is fully loaded
 driver.get("https://www.youtube.com")
 
-time.sleep(20) # wait for page to load
+# Wait for the driver to log more network activities after it's fully loaded
+time.sleep(20)
 
 har = driver.har
 
 timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-outfile   = pathlib.Path(f"youtube_{timestamp}.har.json") 
+outfile = pathlib.Path(f"youtube_{timestamp}.har.json")
 with outfile.open("w", encoding="utf-8") as f:
-    json.dump(har, f, indent=2)
+    f.write(har)
+    f.flush()
 
 print(f"HAR written to {outfile.resolve()}")
 
